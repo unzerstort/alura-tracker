9450144b01a6fe8a01b977e23735d1fd8d67f30d
Sandra Bastos <sandra@vermillion.net.br>
2024-03-06 14:08:13 -0300

    adds files

---
+++ src/components/Box.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="box has-text-weight-bold">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+/* eslint-disable */
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Box",
+
+});
+</script>
+
+<style scoped>
+.box {
+  color: var(--texto-primario);
+  background-color: var(--bg-secundario);
+  
+}
+</style>
---
+++ src/components/TarefaRealizada.vue
@@ -0,0 +1,41 @@
+<template>
+  <Box class="tarefa">
+    <div class="columns">
+      <div class="column is-7">
+        {{ tarefa.descricao || 'Tarefa sem descrição' }}
+      </div>
+      <div class="column">
+        <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos" />
+      </div>
+    </div>
+  </Box>
+</template>
+
+<script lang="ts">
+import { PropType, defineComponent } from 'vue';
+import Cronometro from './Cronometro.vue';
+import ITarefa from '@/interfaces/ITarefa';
+import Box from './Box.vue'
+
+export default defineComponent({
+  name: 'TarefaRealizada',
+  components: {
+    Cronometro,
+    Box,
+  },
+  props: {
+    tarefa: {
+      type: Object as PropType<ITarefa>,
+      required: true,
+    }
+  },
+})
+
+</script>
+<style>
+.tarefa {
+  color: var(--texto-primario);
+  background-color: var(--bg-secundario);
+}
+
+</style>
