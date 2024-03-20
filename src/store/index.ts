faad8630f69771e3e92fa456d6f16e2c6c3c93d0
Sandra Bastos <sandra@vermillion.net.br>
2024-03-20 09:54:19 -0300

    adds tarefas to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,20 @@
 import IProjeto from "@/interfaces/IProjeto";
+import ITarefa from "@/interfaces/ITarefa";
 import { InjectionKey } from "vue";
 import { Store, createStore, useStore as vuexUseStore } from "vuex";
-import { ADICIONA_PROJETO, ALTERA_PROJETO, EXCLUI_PROJETO } from "./tipo-mutacoes";
+import { ADICIONA_PROJETO, ADICIONA_TAREFA, ALTERA_PROJETO, ALTERA_TAREFA, EXCLUI_PROJETO, EXCLUI_TAREFA } from "./tipo-mutacoes";
 
 interface Estado {
     projetos: IProjeto[];
+    tarefas: ITarefa[];
 }
 
 export const key: InjectionKey<Store<Estado>> = Symbol();
 
 export const store = createStore<Estado>({
     state: {
-        projetos: []
+        projetos: [],
+        tarefas: []
     },
     mutations: {
         [ADICIONA_PROJETO] (state, nomeProjeto: string) {
@@ -27,6 +30,17 @@ export const store = createStore<Estado>({
         },
         [EXCLUI_PROJETO] (state, id: string) {
             state.projetos = state.projetos.filter(proj => proj.id != id);
+        },
+        [ADICIONA_TAREFA] (state, tarefa: ITarefa) {
+            tarefa.id = new Date().toISOString();
+            state.tarefas.push(tarefa);
+        },
+        [ALTERA_TAREFA] (state, tarefa: ITarefa) {
+            const index = state.tarefas.findIndex(tarefa => tarefa.id == tarefa.id);
+            state.tarefas[index] = tarefa;
+        },
+        [EXCLUI_TAREFA] (state, id: string) {
+            state.tarefas = state.tarefas.filter(tarefa => tarefa.id != id);
         }
     }
 });
