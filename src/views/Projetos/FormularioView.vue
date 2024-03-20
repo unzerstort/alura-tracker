66f83335de62e7c9e7f8bb294fbeb2e412514501
Sandra Bastos <sandra@vermillion.net.br>
2024-03-20 11:36:07 -0300

    adds mixin

--- src/views/Projetos/FormularioView.vue
+++ src/views/Projetos/FormularioView.vue
@@ -19,8 +19,9 @@
 <script lang="ts">
 import { TipoNotificacao } from '@/interfaces/INotificacao';
 import { useStore } from '@/store';
-import { ADICIONA_PROJETO, ALTERA_PROJETO, NOTIFICAR } from '@/store/tipo-mutacoes';
+import { ADICIONA_PROJETO, ALTERA_PROJETO } from '@/store/tipo-mutacoes';
 import { defineComponent } from 'vue';
+import { notificacaoMixin } from '@/mixins/notificar'
 
 export default defineComponent({
     name: "FormularioView",
@@ -29,6 +30,7 @@ export default defineComponent({
             type: String,
         }
     },
+    mixins: [notificacaoMixin],
     mounted() {
         if (this.id) {
             const projeto = this.store.state.projetos.find(proj => proj.id == this.id);
@@ -51,13 +53,9 @@ export default defineComponent({
                 this.store.commit(ADICIONA_PROJETO, this.nomeProjeto);
             }
             this.nomeProjeto = '';
-            this.store.commit(NOTIFICAR, {
-                titulo: 'Novo projeto foi salvo',
-                texto: 'Pronto, seu projeto já está disponível :)',
-                tipo: TipoNotificacao.SUCESSO,
-            });
+            this.notificar(TipoNotificacao.SUCESSO, 'Excelente!', 'O projeto foi cadastrado com sucesso!');
             this.$router.push('/projetos');
-        }
+        },
     },
     setup() {
         const store = useStore();
