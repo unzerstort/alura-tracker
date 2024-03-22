4a0837b641e22578fa918f3c98859126c5360c6b
Sandra Bastos <sandra@vermillion.net.br>
2024-03-22 11:09:40 -0300

    refactor

--- src/components/TarefaRealizada.vue
+++ src/components/TarefaRealizada.vue
@@ -33,11 +33,15 @@ export default defineComponent({
 			required: true,
 		}
 	},
-	methods: {
-		tarefaClicada() : void {
-			this.$emit('aoTarefaClicada', this.tarefa);
+	setup(props, { emit }) {
+		const tarefaClicada = () : void => {
+			emit('aoTarefaClicada', props.tarefa);
 		}
-	},
+
+		return {
+			tarefaClicada
+		}
+	}
 })
 
 </script>
